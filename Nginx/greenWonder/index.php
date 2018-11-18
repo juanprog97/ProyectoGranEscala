27bdc19c86c5db8d5968d4e02e79dbde931b12cb
Juan Sebastian Rivera <juansebastianrivera@hotmail.com>
2018-11-17 22:01:25 -0500

    Added database creations

--- Nginx/greenWonder/index.php
+++ Nginx/greenWonder/index.php
@@ -1,4 +1,51 @@
 <!DOCTYPE html>
+<?php
+$servername = "19.18.18.4:3306";
+$username = "test";
+$password = "test";
+$dbname = "test";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$createTable = "CREATE TABLE IF NOT EXISTS Departamento (
+idDepartamento INT AUTO_INCREMENT PRIMARY KEY,
+nombreDpto char(13)
+)";
+$createTable1 = "CREATE TABLE IF NOT EXISTS Ciudad (
+  idCiudad INT AUTO_INCREMENT PRIMARY KEY,
+  nombreCiudad char(13),
+  idDepartamento INT,
+  FOREIGN KEY(idDepartamento) REFERENCES Departamento(idDepartamento) ON DELETE CASCADE
+  )";
+
+$createTable2 = "CREATE TABLE IF NOT EXISTS Premios (
+  idPremio INT AUTO_INCREMENT PRIMARY KEY,
+  nombrePremio char(15)  NULL,
+   descripcion varchar(40)  NOT NULL,
+   precio int  NULL
+  )";
+
+$createTable3 = "CREATE TABLE IF NOT EXISTS Usuarios (
+  idUsuario INT AUTO_INCREMENT PRIMARY KEY,
+  nombre char(30)  NOT NULL,
+   cedula int  NOT NULL,
+   direccion char(40)  NOT NULL,
+   celular numeric(11,0)  NOT NULL,
+   email char(30)  NOT NULL,
+   fechaNac date  NOT NULL,
+   puntos int  NOT NULL DEFAULT 0,
+   idCiudad int  NOT NULL,
+   administrador boolean  NOT NULL DEFAULT 0,
+   FOREIGN KEY(idCiudad) REFERENCES Ciudad(idCiudad) ON DELETE CASCADE
+
+  )";
+
+if ($conn->query($createTable) === TRUE)
+if ($conn->query($createTable1) === TRUE)
+if ($conn->query($createTable2) === TRUE)
+if ($conn->query($createTable3) === TRUE)
+?>
 <html>
 <title>ERP-GREEN WONDER</title>
 <meta charset="UTF-8">
@@ -51,10 +98,5 @@ body,h1,h2,h3,h4,h5,h6
 </div>
 <img src="logo.png" width="165px" height="200px" alt="logo" style="padding-top:50px" background-attachment: fixed;/*style="background-color:rgb(227, 238, 248);"*/ >
 <!-- Header -->
-
-
-
-
-
 </body>
 </html>
